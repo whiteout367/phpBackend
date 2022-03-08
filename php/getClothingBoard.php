5d428d829012f7d9fc149976d0f4fe21f611deae
white467 <jwj78772@gmail.com>
2022-03-09 01:07:44 +0900

    add other board

--- php/getClothingBoard.php
+++ php/getClothingBoard.php
@@ -2,7 +2,7 @@
 
 <?php
  // board테이블에서 idx를 기준으로 내림차순해서 5개까지 표시
-   $sql = mq("select * from board order by idx desc limit 0,5"); 
+   $sql = mq("select * from ClothingBoard order by idx desc limit 0,5"); 
      while($board = $sql->fetch_array())
      {
        //title변수에 DB에서 가져온 title을 선택
---
+++ php/getHomeBoard.php
@@ -0,0 +1,20 @@
+<?php include  $_SERVER['DOCUMENT_ROOT']."/DB.php"; ?>
+
+<?php
+ // board테이블에서 idx를 기준으로 내림차순해서 5개까지 표시
+   $sql = mq("select * from ClothingBoard order by idx desc limit 0,5"); 
+     while($board = $sql->fetch_array())
+     {
+       //title변수에 DB에서 가져온 title을 선택
+       $title=$board["title"]; 
+       if(strlen($title)>30)
+       { 
+         //title이 30을 넘어서면 ...표시
+         $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
+       }
+       $list[] =   $board;
+      
+    }
+    
+    echo json_encode($list,JSON_UNESCAPED_UNICODE);
+?>
