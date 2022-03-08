0b3eb187f20778087ea22795ceb0043cc40ddbd1
white467 <jwj78772@gmail.com>
2022-03-09 02:17:42 +0900

    fix addUser.php sql

--- php/addUser.php
+++ php/addUser.php
@@ -6,18 +6,18 @@ include $_SERVER['DOCUMENT_ROOT']."/phpBack/DB.php";
 $id= $_POST['id'];
 $userpw = password_hash($_POST['pw'], PASSWORD_DEFAULT);
 
-
-$sql = "insert into User(Id,Passwd) values($id,$userpw)";
+//DB에서 id와 pass를 문자열로 설정하여 ''를 넣어주어야 쿼리 작동
+$sql = "insert into User(Id,Passwd) values('$id','$userpw')";
 $sql = mq($sql);
 if ($sql === false) {
     echo "저장에 문제가 생겼습니다. 관리자에게 문의해주세요.";
    
 } else {
 ?>
-    <!-- <script>
+    <script>
         alert("회원가입이 완료되었습니다");
         
-    </script> -->
+    </script>
 <?php
 }
 ?>
